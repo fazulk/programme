3b1449fa0edc27fbf6306bc00db2a00289903ae9
fazulk <jfasulkey@gmail.com>
2022-02-22 23:36:00 -0800

    fix: clean it up

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <script setup>
 const { find } = useStrapi4();
 const response = await find("products");
-
+const graphql = useStrapiGraphQL();
+const { data: products } = await useAsyncData("products", () =>
+  graphql(`
+    query {
+      products {
+        data {
+          id
+          attributes {
+            Name
+          }
+        }
+      }
+    }
+  `)
+);
 </script>
 
 <template>
   <div>
-
-    HI
-    <li v-for="(r, i) in response.data">
+    <li v-for="(r, i) in products.data.products.data">
       {{ r.attributes.Name }}
     </li>
   </div>
