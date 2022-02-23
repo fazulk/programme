98e4bb49c1b1c67d4682d75d4f74dc20e7ecd2bd
fazulk <jfasulkey@gmail.com>
2022-02-22 23:29:22 -0800

    feat: only use strapi api

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,14 @@
 <script setup>
-// const { find } = useStrapi4();
-// const response = await find("products");
-const graphql = useStrapiGraphQL();
+const { find } = useStrapi4();
+const response = await find("products");
 
-const products = await graphql(`
-  query {
-    products {
-      data {
-        id
-        attributes {
-          Name
-        }
-      }
-    }
-  }
-`);
 </script>
 
 <template>
   <div>
-    {{ products.data.products.data }}
+
     HI
-    <li v-for="(r, i) in products.data.products.data">
+    <li v-for="(r, i) in response.data">
       {{ r.attributes.Name }}
     </li>
   </div>
