dfae3e17a4098e58abdfdc665a005658fdcedd17
fazulk <jfasulkey@gmail.com>
2022-05-03 10:26:03 -0700

    feat: update typings and ssr

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,35 @@
 <script lang="ts" setup>
 const graphql = useStrapiGraphQL()
 
-const {
-  data: { products },
-} = await graphql(`
-  query {
-    products {
-      data {
-        attributes {
-          Name
-          Price
+interface Products {
+  data: {
+    products: {
+      data: [
+        {
+          attributes: {
+            Name: string
+            Price: string
+          }
+        }
+      ]
+    }
+  }
+}
+
+const { data: products, refresh } = useAsyncData('products', () =>
+  graphql<Products>(`
+    query {
+      products {
+        data {
+          attributes {
+            Name
+            Price
+          }
         }
       }
     }
-  }
-`)
+  `)
+)
 
 const { data: counter1 } = await useAsyncData('counter1', () =>
   $fetch('/api/counter')
@@ -23,11 +38,15 @@ const { data: counter1 } = await useAsyncData('counter1', () =>
 
 <template>
   <div>
-    counter: {{ counter1 }}<br />
-    <ol>
-      <li v-for="(r, i) in products.data" :key="i">
-        {{ r.attributes.Name }} : {{ r.attributes.Price }}
-      </li>
-    </ol>
+    <div>
+      counter: {{ counter1 }}<br />
+      <ol>
+        <li v-for="(r, i) in products.data.products.data" :key="i">
+          {{ r.attributes.Name }} : {{ r.attributes.Price }}
+        </li>
+      </ol>
+    </div>
+    <br />
+    <button @click.prevent="refresh()">Refresh</button>
   </div>
 </template>
